19b698724dc1ef4f5b2059da55dedc7992ef24a6
[Harshil Karir] <[harshilkarir72@gmail.com]>
2024-02-26 19:03:40 +0530

    gamepage styling done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Sqaure from "./components/sqaure/Sqaure";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  let sqaures = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  const [gameState, setGameState] = useState(sqaures);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="move-detection">
+        <div className="left">Yourself</div>
+        <div className="right">Opponent</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="game-heading water-bg">Tic Tac Toe</div>
+      <div className="sqaure-wrapper">
+        {gameState.map((item) => {
+          return item.map((e) => {
+            return <Sqaure setGameState={setGameState} id={e} key={e} />;
+          });
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/sqaure/Sqaure.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sqaure.css";
+
+const Sqaure = ({ setGameState, id }) => {
+  return <div className="sqaure"></div>;
+};
+
+export default Sqaure;
